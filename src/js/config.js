7b58b2aa0a45b925a84ca566b11e22f3bd369c4a
miamai <cb121466@gmail.com>
2022-11-02 14:08:55 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '062ab715-a77f-485a-9823-385f33f85665';
+export const MODAL_CLOSE_SEC = 2.5;
